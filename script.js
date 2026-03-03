a8acdbfca4366ee13fbfce78db641b6b33fd0a56
Louisemonte03 <louisemonte03@gmail.com>
2026-03-03 15:09:08 +0100

    update website

---
+++ script.js
@@ -0,0 +1,14 @@
+let inputVeld = document.getElementById("vestigingsplaats");
+let dataLijst = document.getElementById("gemeente-opties");
+
+// json bestand inladen
+fetch("gemeentes.json")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+    data.forEach((gemeente) => {
+      const option = document.createElement("option");
+      option.value = typeof gemeente === "string" ? gemeente : gemeente.naam;
+      dataLijst.appendChild(option);
+    });
+  });
